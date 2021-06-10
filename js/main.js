70f1e3dd520035ef311f9a160d399cf7b5de12f7
Emily Ha <emily.h@uconn.edu>
2021-06-10 16:11:36 -0400

    add all

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
-/**
- * AOS v2 Initialization
- * @url https://github.com/michalsnik/aos/tree/v2#init-aos
- */
-AOS.init({
-    duration: 1000,
-    disable: Boolean(window.innerWidth < 1024),
-    delay: 200,
-    once: true
-  });
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
